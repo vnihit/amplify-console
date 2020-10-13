5f00be32eeae5cf6dab4b92730da212b5d7ab74b
Nihit Vyas <vnihit@gmail.com>
2020-10-13 15:04:12 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Nihit FINAL
+           Develop branch FINAL
           </p>
           <a
             className="App-link"
